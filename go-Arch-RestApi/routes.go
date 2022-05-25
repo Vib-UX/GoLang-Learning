ca5215c04f1200d64926b54be7c35883a4fb00e2
Vib-UX <vibchess123@gmail.com>
2022-05-25 11:41:48 +0530

    routes update for entity and repo

--- go-Arch-RestApi/routes.go
+++ go-Arch-RestApi/routes.go
@@ -2,45 +2,40 @@ package main
 
 import (
 	"encoding/json"
+	"math/rand"
 	"net/http"
+
+	"./entity"
+	"./repository"
 )
 
-type Post struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"text"`
-}
-
-var posts []Post
-
-func init() {
-	posts = []Post{{1, "title 1", "text 1"}}
-}
+var repo repository.PostRepository = repository.NewPostRepository()
 
 func getPosts(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
-	result, err := json.Marshal(posts)
+	posts, err := repo.FindAll()
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{"error":"Error marshalling data"}`))
+		response.Write([]byte(`{"error":"Error getting the data from repo"}`))
 	}
 	response.WriteHeader(http.StatusOK)
-	response.Write(result)
+	json.NewEncoder(response).Encode(posts)
 }
 
 func addPost(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
-	var post Post
+	var post entity.Post
 	err := json.NewDecoder(request.Body).Decode(&post)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte(`{"error":"Error unmarshalling data"}`))
 		return
 	}
-	post.Id = len(posts) + 1
-	posts = append(posts, post)
+	post.Id = rand.Int()
+	repo.Save(&post)
 	response.WriteHeader(http.StatusOK)
 	result, err := json.Marshal(post)
 	response.Write(result)
+	json.NewEncoder(response).Encode(post)
 
 }
