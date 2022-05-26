a0b7dec4a15a9d21a6ae892e96def7232549b69a
Vib-UX <vibchess123@gmail.com>
2022-05-26 12:39:29 +0530

    Resolving iterator object

--- go-Arch-RestApi/repository/post-repo.go
+++ go-Arch-RestApi/repository/post-repo.go
@@ -6,6 +6,7 @@ import (
 
 	"../entity"
 	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
 )
 
 type PostRepository interface {
@@ -44,4 +45,35 @@ func (*repo) Save(post *entity.Post) (*entity.Post, error) {
 	}
 
 	return post, nil
+}
+
+func (*repo) FindAll() ([]entity.Post, error) {
+	//Create an empty context that needs to passed as parameter to the firestore client
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+	var posts []entity.Post
+	it := client.Collection(collectionName).Documents(ctx)
+	for {
+		doc, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate the list of posts: %v", err)
+			return nil, err
+		}
+		post := entity.Post{
+			Id:    doc.Data()["Id"].(int64),
+			Title: doc.Data()["Title"].(string),
+			Text:  doc.Data()["Text"].(string),
+		}
+
+		posts = append(posts, post)
+	}
+
+	return posts, nil
 }
