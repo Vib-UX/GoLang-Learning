1a06ca3c4abdd5f9e0230c628fc4d40415240965
Vib-UX <vibchess123@gmail.com>
2022-05-30 16:51:58 +0530

    MVC model

---
+++ go-Arch-RestApi/controller/post-controller.go
@@ -0,0 +1,63 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"../entity"
+	"../errors"
+	"../service"
+)
+
+type controller struct{}
+
+var (
+	postService service.PostService
+)
+
+type PostController interface {
+	GetPosts(response http.ResponseWriter, request *http.Request)
+	AddPost(response http.ResponseWriter, request *http.Request)
+}
+
+func NewPostController(service service.PostService) PostController {
+	postService = service
+	return &controller{}
+}
+
+func (*controller) GetPosts(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	posts, err := postService.FindAll()
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Error getting the posts"})
+	}
+	response.WriteHeader(http.StatusOK)
+	json.NewEncoder(response).Encode(posts)
+}
+
+func (*controller) AddPost(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var post entity.Post
+	err := json.NewDecoder(request.Body).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Error unmarshalling data"})
+		return
+	}
+	err1 := postService.Validate(&post)
+	if err1 != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: err1.Error()})
+		return
+	}
+
+	result, err2 := postService.Create(&post)
+	if err2 != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Error saving the post"})
+		return
+	}
+	response.WriteHeader(http.StatusOK)
+	json.NewEncoder(response).Encode(result)
+}
